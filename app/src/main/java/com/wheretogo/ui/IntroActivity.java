65bbafeae5cd1a05c8d6b30941cd1932b0a1658a
Maksim Shengeliia <maksim.shengeliia@mail.ru>
2020-04-01 02:38:10 +0300

    Добавил IntroActivity

---
+++ app/src/main/java/com/wheretogo/ui/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.wheretogo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
